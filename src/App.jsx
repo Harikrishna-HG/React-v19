e66960cd59eee8ed0f5daeafbf922579cf44b1af
Harikrishna Gautam <93207808+Harikrishna-HG@users.noreply.github.com>
2025-05-03 17:54:06 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//subscribe to Thapa Technical Youtube Channel - https://www.youtube.com/thapatechnical
+
+import { ReactMemo } from "./hooks/Memo/ReactMemo";
+import NetflixSeries from "./components/NetflixSeries";
+
+export const App = () => {
+  return (
+    <>
+      <ReactMemo />
+      <NetflixSeries />
+    </>
+  );
+};
